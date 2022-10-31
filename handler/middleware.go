f3d8dcc7fe80cb0a8d868403b8852199194c22c1
KOngTNP <tanapollll23@gmail.com>
2022-10-31 12:43:24 +0700

    add Authorization APIKEY

---
+++ handler/middleware.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func ValidateAPIKey(apiKey string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.GetHeader("Authorization") != apiKey {
+			errMessage := "APIKEY not found"
+			logrus.Warn(errMessage, ", APIKEY: ", c.GetHeader("Authorization"))
+			c.JSON(http.StatusUnauthorized, gin.H{"status": 401, "message": "Authentication failed"})
+			c.Abort()
+			return
+		}
+	}
+}
