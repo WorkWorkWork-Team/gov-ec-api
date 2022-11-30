0bd8bd6b3fbbfb1abce46b37dfe5d6a4a029a492
vitvara <gridxlgm@gmail.com>
2022-12-01 00:34:41 +0700

    add integration test

--- handler/population_test.go
+++ handler/population_test.go
@@ -6,30 +6,22 @@ import (
 
 	"github.com/WorkWorkWork-Team/gov-ec-api/handler"
 	"github.com/WorkWorkWork-Team/gov-ec-api/model"
+	"github.com/WorkWorkWork-Team/gov-ec-api/repository"
 	"github.com/WorkWorkWork-Team/gov-ec-api/service"
-	"github.com/WorkWorkWork-Team/gov-voter-api/handler"
-	model "github.com/WorkWorkWork-Team/gov-voter-api/models"
-	"github.com/WorkWorkWork-Team/gov-voter-api/repository"
-	"github.com/WorkWorkWork-Team/gov-voter-api/service"
 	"github.com/gin-gonic/gin"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var (
-	TestUserCitizenID   string = "1234567891235"
-	TestJWTSecretKey    string = "key"
-	TestJWTIssuer       string = "Tester"
-	TestJWTTTL          int    = 10
-	UserHandler         *handler.UserHandler
-	AuthenticateHandler *handler.AuthenticateHandler
+	PopulationHander *handler.PopulationHandler
 )
 
 var _ = Describe("User Integration Test", Label("integration"), func() {
 	BeforeEach(func() {
 		populationRepository := repository.NewPopulationRepository(MySQLConnection)
 		populationService := service.NewPopulationService(populationRepository)
-		PopulationHandler := handler.NewPopulationHandler(populationService)
+		PopulationHandler = handler.NewPopulationHandler(populationService)
 	})
 
 	Context("Population Info API", func() {
@@ -38,18 +30,16 @@ var _ = Describe("User Integration Test", Label("integration"), func() {
 				It("should return success.", func() {
 					// Expect no user in voted table
 					var populationList []model.PopulationDatabaseRow
-					err := MySQLConnection.Select(&populationList, "SELECT * FROM Population")
-					Expect(err).ShouldNot(HaveOccurred())
+					row := MySQLConnection.Select(&populationList, "SELECT * FROM Population")
+					Expect(row).ShouldNot(HaveOccurred())
 					populationLength := len(populationList)
-					Expect(populationLength).To(Equal(0))
+					Expect(populationLength).To(Equal(2))
 
 					res := httptest.NewRecorder()
-					c, r := gin.CreateTestContext(res)
+					c, _ := gin.CreateTestContext(res)
 					c.Request = httptest.NewRequest(http.MethodPost, "/api", nil)
-
-					r.POST("/api", PopulationHandler.GetPopulationStatistics)
-
-					r.ServeHTTP(res, c.Request)
+					PopulationHandler.GetPopulationStatistics(c)
+					Expect(res.Result().StatusCode).To(Equal(http.StatusOK))
 				})
 			})
 		})
