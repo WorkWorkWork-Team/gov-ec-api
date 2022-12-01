96ca102ccbd783b16a0f128f1151a830273f3e4e
Pakapop Cheunchomsirakul <abounk@Pakapops-MacBook-Pro.local>
2022-12-01 23:20:00 +0700

    add unit test for submitmp

---
+++ service/submitmp_test.go
@@ -0,0 +1,59 @@
+package service_test
+
+import (
+	"github.com/WorkWorkWork-Team/gov-ec-api/service"
+	"github.com/WorkWorkWork-Team/gov-ec-api/test/mock_repository"
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Population", Label("unit"), func() {
+	var ctrl *gomock.Controller
+	var mockSubmitmpRepository *mock_repository.MockSubmitMpRepository
+	var mockPopulationRepository *mock_repository.MockPopulationRepository
+	var submitmpService service.SubmitmpService
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		mockSubmitmpRepository = mock_repository.NewMockSubmitMpRepository(ctrl)
+		mockPopulationRepository = mock_repository.NewMockPopulationRepository(ctrl)
+		submitmpService = service.NewSubmitmpService(mockSubmitmpRepository, mockPopulationRepository)
+	})
+
+	Describe("Submit MP", func() {
+		Context("When person does exists", func() {
+			// var person *model.PopulationDatabaseRow
+
+			BeforeEach(func() {
+				// person = &model.PopulationDatabaseRow{
+				// 	CitizenID:   1,
+				// 	LazerID:     "1",
+				// 	Name:        "Johnny",
+				// 	Lastname:    "J",
+				// 	Birthday:    time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local),
+				// 	Nationality: "eSan",
+				// 	DistrictID:  "101",
+				// }
+				mockPopulationRepository.EXPECT().CheckIfPeopleExists("1").Return(true)
+				mockSubmitmpRepository.EXPECT().SubmitMpToDB("1").Return(nil)
+			})
+
+			It("Should not return ErrCitizenIDNotFound", func() {
+				err := submitmpService.SubmitMp("1")
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		Context("When the person doesn't exists", func() {
+			BeforeEach(func() {
+				mockPopulationRepository.EXPECT().CheckIfPeopleExists("2").Return(false)
+			})
+
+			It("Should return error", func() {
+				err := submitmpService.SubmitMp("2")
+				Expect(err).Should(Equal(service.ErrCitizenIDNotFound))
+			})
+		})
+	})
+})
